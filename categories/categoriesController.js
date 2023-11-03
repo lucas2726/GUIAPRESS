e27bba7a79a03661e58e09f51ce8ed4df296e138
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-03 18:47:15 -0400

    ex

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const router = express.Router()
-
+const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req,res) => {
-    res.send("admin/categories/new")
+    res.render("admin/categories/new")
+})
+
+router.post("/categories/save", (req, res) => {
+    let title = req.body.title
+    if(title != undefined) { 
+
+       Category.create({
+        title: title,
+        slug:slugify(title) //"Computação e informatica" => "computacao-e-informatica"(Transforma em url)
+       }).then(() => {
+         res.redirect("/")
+       })
+    } else {
+        res.redirect("/admin/categories/new")
+    }
 })
 
 module.exports = router
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const connection = new Sequelize ('guiapress', 'root', 'root' ,{
 host: 'localhost',
-dialect: 'mysql'
-
+dialect: 'mysql',
+logging: false
+})
 
   module.exports = connection
