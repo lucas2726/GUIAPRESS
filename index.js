5b597d1882f884477165f99564068f473a4fcbd2
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-03 08:09:06 -0400

    ex

---
+++ index.js
@@ -0,0 +1,17 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const ejs = require('ejs')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.send("")
+})
+
+app.listen(8080, () => {
+    console.log("O servidor está rodando!")
+})
