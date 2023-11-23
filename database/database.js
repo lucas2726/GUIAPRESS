ef9269f84664838234456254ad0dbfe846b99dc0
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-22 20:17:00 -0400

    ws

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize ('guiapress', 'root', 'root' ,{
-host: 'localhost',
-dialect: 'mysql',
-timezone: "-03:00",
-logging: false
+const connection = new Sequelize('guiapress', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  timezone: "-03:00",
+  logging: false
 })
 
-  module.exports = connection
+module.exports = connection
